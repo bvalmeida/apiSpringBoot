aa5652dd9e888fcf6f18dcc6d87a364a3eeb7883
Breno Vieira <bvalmeida94@gmail.com>
2022-01-19 13:50:27 -0300

    segunda tentativa

--- combate-covid/api/src/main/java/br/com/covid/api/controller/HospitalController.java
+++ combate-covid/api/src/main/java/br/com/covid/api/controller/HospitalController.java
@@ -11,14 +11,12 @@ public class HospitalController {
     private final InputPort inputPort;
 
     public HospitalController(InputPort inputPort) {
-
         this.inputPort = inputPort;
     }
 
     @PostMapping
     public void criarHospital(@RequestBody HospitalRequest request){
         this.inputPort.salvarHospital(request.convertHospitalInputPort(request));
-
     }
 
 
